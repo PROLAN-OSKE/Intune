c896268a0416c004e274ab950be6d9a78de2b737
Ronald Oske <oske@prolan.de>
2026-01-02 17:00:18 +0100

    Add files via upload

---
+++ Vorlagen/Taskbar/Taskbar-Aktive-Anwendung_remediation.ps1
@@ -0,0 +1,21 @@
+﻿#Fileext
+$regkey="HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced\"
+$name="ShowTaskViewButton"
+$value=0
+
+#Registry Template
+If (!(Test-Path $regkey))
+{
+New-Item -Path $regkey -ErrorAction stop
+}
+
+if (!(Get-ItemProperty -Path $regkey -Name $name -ErrorAction SilentlyContinue))
+{
+New-ItemProperty -Path $regkey -Name $name -Value $value -PropertyType DWORD -ErrorAction stop
+write-output "remediation complete"
+exit 0
+}
+
+set-ItemProperty -Path $regkey -Name $name -Value $value -ErrorAction stop
+write-output "remediation complete"
+exit 0
